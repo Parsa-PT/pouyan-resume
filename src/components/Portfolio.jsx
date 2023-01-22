96742719e342ad46c2de80090a59df5477feb894
Parsapt <90087314+Parsa-PT@users.noreply.github.com>
2023-01-22 22:45:02 +0330

    Update Portfolio.jsx

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -6,12 +6,14 @@ import Scroll from './Scroll'
 
 const Portfolio = () => {
 
+const Portfolio = () => {
+
   const project = [
     {
       id: 1,
       title : 'BookRun',
       url : fristpic,
-      address : 'https://github.com/Pouyanjh/BookRun'
+      address : 'https://github.com/Pouyanjh/BookRun',
     },
     {
       id: 2,
@@ -23,7 +25,8 @@ const Portfolio = () => {
       id: 3,
       title : 'Tech-finder',
       url : latspic,
-      address : 'https://github.com/Pouyanjh/TechFinder'
+      address : 'https://github.com/Pouyanjh/TechFinder',
+      liveMode: 'https://pouyandev1.pythonanywhere.com'
     },
   ]
   return (
@@ -36,7 +39,10 @@ const Portfolio = () => {
               <div className='w-full overflow-hidden rounded-lg  gap-4 flex flex-col items-center bg-[#2d0c658f]'>
                   <img className=' hover:scale-105 duration-150' src={item.url} alt="mypic" />
                   <p className='text-white'>{item.title}</p>
+                   <div className='lg:flex lg:gap-4 px-3'>
                   <a target={'_blank'} rel="noreferrer" className='btn-bg w-[10rem] mb-3 text-white px-4 py-2 rounded-md flex  justify-center' href={item.address}>GitHub </a>
+                  <a target={'_blank'}  rel="noreferrer" className='btn-bg w-[10rem] mb-3 text-white px-4 py-2 rounded-md flex  justify-center' href={item.liveMode}>Live </a>
+                  </div>
               </div>
             )
           })}
